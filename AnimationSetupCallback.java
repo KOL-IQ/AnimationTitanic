9c2d5b4ef5a646617081b059044bcc39bcf1004f
KOL-IQ <bromongkoliq@gmail.com>
2026-07-05 16:08:51 +0700

    java/com/android/animation

---
+++ AnimationSetupCallback.java
@@ -0,0 +1,9 @@
+package com.android.animation;
+
+import com.android.modmenu.Menu;
+
+public interface AnimationSetupCallback {
+    public void onSetupAnimation(TitanicTextView titanicTextView);
+    public void onSetupAnimation(TitanicButton Button);
+}
+
